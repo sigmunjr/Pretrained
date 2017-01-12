68b24fb876d8b424fa425af6aab68f5cfb99654f
Sigmund Rolfsjord <sigmunjr@ifi.uio.no>
2017-01-12 16:38:22 +0100

    Tullete feil som gjorde at man ikke kunne se weight_img

--- test_pretrained.py
+++ test_pretrained.py
@@ -20,7 +20,7 @@ with slim.arg_scope(nets.resnet_v1.resnet_arg_scope(is_training=False)):
   raw_output, end_points = nets.resnet_v1.resnet_v1_50(image[tf.newaxis, :, :, :], num_classes=1000)
 predictions = end_points['predictions']
 after_first_resnet_unit = end_points['resnet_v1_50/block1/unit_1/bottleneck_v1']
-spatial_first_weight = tf.get_default_graph().get_tensor_by_name('resnet_v1_50/block1/unit_1/bottleneck_v1/conv3/weights:0')
+spatial_first_weight = tf.get_default_graph().get_tensor_by_name('resnet_v1_50/block1/unit_1/bottleneck_v1/conv2/weights:0')
 
 #Load pretrained weights
 saver = tf.train.Saver(var_list=tf.global_variables())
@@ -38,9 +38,9 @@ for img_name in os.listdir('./images'):
     for j in range(8):
       plt.figure(1)
       plt.subplot(8, 8, j + i*8 + 1)
-      plt.imshow(weight_img[:, :, 32, j + i*j], vmin=filter_img[0, :, :, j + i*j].min(), vmax=filter_img[0, :, :, j + i*j].max())
+      plt.matshow(weight_img[:, :, 32, j + i*8], vmin=weight_img[:, :, 32, j + i*8].min(), vmax=weight_img[:, :, 32, j + i*8].max(), interpolation='none')
       plt.figure(2)
       plt.subplot(8, 8, j + i*8 + 1)
-      plt.imshow(filter_img[0, :, :, j + i*j])
+      plt.imshow(filter_img[0, :, :, j + i*8])
   plt.show()
   print(img_name, 'classified as:', label_name)
